182bfd426881b529ac4f8c019272e6f56b9600f8
martosv <virginia.martos.muoz@gmail.com>
2015-06-03 16:40:35 +0200

    Primeros botones de propiedades

--- app/assets/javascripts/elements.js
+++ app/assets/javascripts/elements.js
@@ -1,2 +1,36 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+
+	var elements;
+
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:3000/elements",
+		success: function(response){elements = response},
+		error: function(response){alert("Success: false")},
+		dataType: "json"
+	});
+
+
+
+	$("#mass").on("click", function(){
+		for(var i = 0; i < elements.length; i++ ) {
+			console.log(i, elements[i].mass)
+			if (elements[i].mass != null)
+				$("#element"+ (i+1) +"-property").text(elements[i].mass);
+			else 
+				$("#element"+ (i+1) +"-property").text(' ');
+		}
+	});
+
+	$("#boiling-point").on("click", function(){
+		for(var i = 0; i < elements.length; i++ ) {
+			console.log(i, elements[i].boiling_point)
+			if (elements[i].boiling_point != null)
+				$("#element"+ (i+1) +"-property").text(elements[i].boiling_point);
+			else 
+				$("#element"+ (i+1) +"-property").text(' ');
+		}
+	});
+
+});
--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -12,7 +12,19 @@ class ElementsController < ApplicationController
 			[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 			[0,0,5,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 			[0,0,5,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
+			respond_to do |format|
+				format.html
+				format.json{ render json: @elements}
+			end
+
 		
 		#@elements_alkali_metals	
+	end
+
+	def property(property)
+		@elements = Element.all
+		@properties = @elements.property
+		render json: @properties
 	end
 end
