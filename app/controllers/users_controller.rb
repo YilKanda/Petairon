9949d65b0efb2e933afaca2ce45ac84f42867057
martosv <virginia.martos.muoz@gmail.com>
2015-07-13 13:29:53 +0200

    Footer arreglado y creación de usuarios arreglada

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    @image_size = File.size(params[:user][:image].tempfile)
-    if @image_size < 150000
+    if params[:user][:image]
+      @image_size = File.size(params[:user][:image].tempfile)
+      if @image_size < 150000
+        if @user.save
+          flash[:notice] = "User created successfully. Log in!"
+          redirect_to login_path
+        else
+          flash[:error] = @user.errors.full_messages
+          redirect_to new_user_path
+        end
+      else
+        flash[:alert] = 'Image size is too big!'
+        redirect_to new_user_path
+      end
+    else
       if @user.save
         flash[:notice] = "User created successfully"
         redirect_to login_path
@@ -30,9 +43,6 @@ class UsersController < ApplicationController
         flash[:error] = @user.errors.full_messages
         redirect_to new_user_path
       end
-    else
-      flash[:alert] = 'Image size is too big!'
-      redirect_to new_user_path
     end
   end
 
@@ -42,8 +52,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find params[:id]
-    @image_size = File.size(params[:user][:image].tempfile)
-    if @image_size < 150000
+    if params[:user][:image]
+      @image_size = File.size(params[:user][:image].tempfile)
+      if @image_size < 150000
+        if @user.update user_params
+          flash[:notice] = "User updated successfully!"
+          redirect_to user_path(@user)
+        else
+          flash[:error] = @user.errors.full_messages
+          redirect_to edit_user_path(@user)
+        end
+      else
+        flash[:alert] = 'Image size is too big!'
+        redirect_to edit_user_path(@user)
+      end
+    else
       if @user.update user_params
         flash[:notice] = "User updated successfully!"
         redirect_to user_path(@user)
@@ -51,9 +74,6 @@ class UsersController < ApplicationController
         flash[:error] = @user.errors.full_messages
         redirect_to edit_user_path(@user)
       end
-    else
-      flash[:alert] = 'Image size is too big!'
-      redirect_to edit_user_path(@user)
     end
   end
 
